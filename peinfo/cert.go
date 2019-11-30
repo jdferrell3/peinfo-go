fead5b06aaa6ec92ad0f3e64d60ffa39073e8317
John Ferrell <jdferrell3@gmail.com>
2019-11-30 18:12:29 -0500

    validate expired cert chain

--- peinfo/cert.go
+++ peinfo/cert.go
@@ -3,13 +3,19 @@ package peinfo
 import (
 	// "bytes"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"debug/pe"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"path/filepath"
 
 	// "github.com/fullsailor/pkcs7"
-	"go.mozilla.org/pkcs7"
+	// "go.mozilla.org/pkcs7"
+	"github.com/jdferrell3/pkcs7"
 )
 
 const (
@@ -65,21 +71,28 @@ func readCert(fh *os.File, offset int64, size int64) (cert CertDetails, err erro
 	return c, nil
 }
 
-func (f *FileT) VerifyCert() (cert *x509.Certificate, verified bool, err error) {
+func (f *FileT) VerifyCert(validateExpiredChain bool) (cert *x509.Certificate, verified bool, expired bool, err error) {
+	expired = true
+
 	idd := f.FindDataDirectory(pe.IMAGE_DIRECTORY_ENTRY_SECURITY)
 	if f.Verbose {
 		fmt.Printf("IMAGE_DIRECTORY_ENTRY_SECURITY virtual address: %d\n", idd.VirtualAddress)
 		fmt.Printf("IMAGE_DIRECTORY_ENTRY_SECURITY size: %d\n", idd.Size)
 	}
 
+	if int64(idd.VirtualAddress) == 0 {
+		err = fmt.Errorf("IMAGE_DIRECTORY_ENTRY_SECURITY not found")
+		return nil, false, expired, err
+	}
+
 	c, err := readCert(f.OSFile, int64(idd.VirtualAddress), int64(idd.Size))
 	if nil != err {
 		err = fmt.Errorf("readCert failed: %s", err)
-		return nil, false, err
+		return nil, false, expired, err
 	}
 
 	if c.CertificateType != WIN_CERT_TYPE_PKCS_SIGNED_DATA {
-		return nil, false, fmt.Errorf("only pkcs certificates supported (cert type = %d)", c.CertificateType)
+		return nil, false, expired, fmt.Errorf("only pkcs certificates supported (cert type = %d)", c.CertificateType)
 	}
 
 	if f.ExtractCert {
